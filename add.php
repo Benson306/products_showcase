9d33a933561fc70eb9d3ba9ae85b3c4606240eb1
Benson Ndiwa <bnkimtai@gmail.com>
2023-07-02 23:05:16 +0300

    Image Displayed

--- add.php
+++ add.php
@@ -17,7 +17,7 @@ require("connection.php");
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
                   Add Product
               </h1>
-              <form class="space-y-4 md:space-y-6" action="add_action.php" method="POST">
+              <form class="space-y-4 md:space-y-6" action="add_action.php" method="POST" enctype="multipart/form-data">
                   <div>
                       <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Title</label>
                       <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="HP Folio 9470m" required="">
@@ -31,8 +31,8 @@ require("connection.php");
                       <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="45000" required="">
                   </div>
                   <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Select Image</label>
-                        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" required>
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Select Image</label>
+                        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file" type="file" name="file" required>
                   </div>
                   <button type="submit" id="add" name="add" class="w-full text-white bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800">Add Product</button>
               </form>
--- add_action.php
+++ add_action.php
@@ -6,9 +6,16 @@ if(isset($_POST['add'])){
     $details = mysqli_real_escape_string($conn,strip_tags($_POST['details']));
     $price = mysqli_real_escape_string($conn,strip_tags($_POST['price']));
 
+    $img = $_FILES['file']['name'];
 
-	$ins_sql =  "INSERT INTO products (title, details, price, image) VALUES ('$title','$details','$price', 'img')"; 
-	if(mysqli_query($conn,$ins_sql)){?>
+
+	$ins_sql =  "INSERT INTO products (title, details, price, image) VALUES ('$title','$details','$price', '$img')"; 
+	if(mysqli_query($conn,$ins_sql)){
+        
+        move_uploaded_file($_FILES['file']['tmp_name'], "images/$img");
+        
+        ?>
+
 		<script>
 		window.alert("Product Added");
 		window.location="add.php";
